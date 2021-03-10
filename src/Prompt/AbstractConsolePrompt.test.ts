2eee5c304f52e0edc2b85d2f97581da9c1139230
Franck DIOMANDE <fkdiomande.dev@gmail.com>
2021-03-10 14:02:00 +0100

    test(prompt): add tests for abstract console prompt

---
+++ src/Prompt/AbstractConsolePrompt.test.ts
@@ -0,0 +1,54 @@
+import {assertEquals, Input, Mock} from "../deps.ts";
+import {AbstractConsolePrompt} from "./mod.ts";
+
+class FakePrompt extends AbstractConsolePrompt {
+  constructor() {
+    super();
+  }
+}
+
+Deno.test("Altdx Console Prompt - Should set and get prompt type", () => {
+  const prompt = new FakePrompt();
+  assertEquals(true, prompt.setType(Input) instanceof FakePrompt);
+  assertEquals(Input, prompt.getType());
+});
+
+Deno.test("Altdx Console Prompt - Should set and get prompt name", () => {
+  const prompt = new FakePrompt();
+  assertEquals(true, prompt.setName("name") instanceof FakePrompt);
+  assertEquals("name", prompt.getName());
+});
+
+Deno.test("Altdx Console Prompt - Should set and get prompt message", () => {
+  const prompt = new FakePrompt();
+  assertEquals(true, prompt.setMessage("Enter your name") instanceof FakePrompt);
+  assertEquals("Enter your name", prompt.getMessage());
+});
+
+Deno.test("Altdx Console Prompt - Should get right options", () => {
+  const prompt = new FakePrompt();
+  assertEquals({
+    type: Input,
+    name: "answer",
+    message: "",
+  }, prompt.getQuestion());
+});
+
+Deno.test("Altdx Console Prompt - Should get right parse options", () => {
+  const prompt = new FakePrompt();
+  assertEquals({
+    type: Input,
+    name: "answer",
+    message: "",
+  }, prompt.getParseQuestion());
+});
+
+Deno.test("Altdx Console Prompt - Should prompt question", async () => {
+  const mock = new Mock();
+  mock.spyOn(Input, "prompt");
+
+  const prompt = new FakePrompt();
+  await prompt.prompt();
+
+  assertEquals(true, mock.haveBeenCalledWith(prompt.getParseQuestion()));
+});
